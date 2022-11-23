90a36c12c064049ccc646c14d8b2fed829dba429
Kevin Silvester <kevinsilvester05@gmail.com>
2022-11-23 22:52:52 +0000

    feat: made reset scripts

---
+++ scripts/reset.ps1
@@ -0,0 +1,2 @@
+rm -r -fo "$env:LOCALAPPDATA\temp\nvim"
+rm -r -fo "$env:LOCALAPPDATA\nvim-data"
---
+++ scripts/reset.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
