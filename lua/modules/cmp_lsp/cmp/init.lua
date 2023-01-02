29c9c92e07cb1a238f9c3a1c13a22f5ba2fa8c9c
Kevin Silvester <kevinsilvester05@gmail.com>
2023-01-02 15:48:27 +0000

    fix(cmp): remove has_word_before check

--- lua/modules/cmp_lsp/cmp/init.lua
+++ lua/modules/cmp_lsp/cmp/init.lua
@@ -80,9 +80,9 @@ cmp.setup({
 
    sources = {
       { name = 'crates', group_index = 1 },
+      { name = 'copilot', max_item_count = 3, group_index = 2 },
       { name = 'nvim_lsp', group_index = 1 },
       -- { name = 'nvim_lsp_signature_help', group_index = 2 },
-      { name = 'copilot', max_item_count = 3, group_index = 2 },
       { name = 'nvim_lua', group_index = 2 },
       { name = 'luasnip', group_index = 3 },
       {
@@ -121,7 +121,7 @@ cmp.setup({
          c = cmp.mapping.close(),
       }),
       ['<Tab>'] = cmp.mapping(function(fallback)
-         if cmp.visible() and has_words_before() then
+         if cmp.visible() then
             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
          elseif luasnip.jumpable(1) then
             luasnip.jump(1)
