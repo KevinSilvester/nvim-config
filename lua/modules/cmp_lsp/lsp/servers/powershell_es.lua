4bbf311b29c15b05a94d8ebecd58b8b7ded21f6f
Kevin Silvester <kevinsilvester05@gmail.com>
2023-03-20 13:33:39 +0000

    fix(lsp): fix powershell_es start command

--- lua/modules/cmp_lsp/lsp/servers/powershell_es.lua
+++ lua/modules/cmp_lsp/lsp/servers/powershell_es.lua
@@ -1,39 +1,23 @@
 local ufs = require('utils.fs')
 local M = {}
 
-local PATH = '/mason/packages/powershell-editor-services/PowerShellEditorServices'
-
-local extend = function(t, ...)
-   for i = 1, select('#', ...) do
-      local x = select(i, ...)
-      if x then
-         for k, v in pairs(x) do
-            t[k] = v
-         end
-      end
-   end
-   return t
-end
+local PATH = { 'mason', 'packages', 'powershell-editor-services', 'PowerShellEditorServices' }
 
 local bundle_path = function(p)
-   p = p or ''
-   local tbl = { vim.fn.stdpath('data') }
-   extend(tbl, vim.split(PATH, '/', {}))
-   extend(tbl, { p })
-   return ufs.path_join(unpack(tbl))
+   return ufs.path_join(vim.fn.stdpath('data'), table.concat(PATH, ufs.path_separator), p)
 end
 
 local log_path = function(file_name)
    return ufs.path_join(vim.fn.stdpath('cache'), file_name)
 end
 
-local pses_command = bundle_path('/Start-EditorServices.ps1')
+local pses_command = bundle_path('Start-EditorServices.ps1')
    .. ' -BundledModulesPath '
    .. bundle_path()
    .. ' -LogPath '
-   .. log_path('/pses.log')
+   .. log_path('pses.log')
    .. ' -SessionDetailsPath '
-   .. log_path('/pses-session.json')
+   .. log_path('pses-session.json')
    .. " -FeatureFlags @() -AdditionalModules @() -HostName 'My Client' -HostProfileId 'myclient' -HostVersion 1.0.0 -Stdio -LogLevel Normal"
 
 M.bundle_path = bundle_path()
