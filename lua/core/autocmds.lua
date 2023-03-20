d869fd77f4b65c4623e3e78dd47d93e7c5ae93d2
Kevin Silvester <kevinsilvester05@gmail.com>
2023-03-20 13:48:45 +0000

    style: format

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -3,8 +3,8 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
    pattern = { 'qf', 'help', 'man', 'lspinfo', 'spectre_panel', 'lir' },
    callback = function()
       vim.cmd([[
-      nnoremap <silent> <buffer> q :close<CR> 
-      set nobuflisted 
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
     ]])
    end,
 })
