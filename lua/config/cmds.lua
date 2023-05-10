05d2d94e29a4c7d7b02ad3df14b9ca8b3320213f
KevinSilvester <kevinsilvester05@gmail.com>
2023-05-10 16:03:48 +0100

    feat(cmd): make command to update tabstop

--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -1,5 +1,3 @@
-local colours = require('utils.colours')
-
 -- command to act as alias for custom inspect function
 vim.api.nvim_create_user_command('Inspect', function(opts)
    local args = vim.split(opts.args, '|')
@@ -22,4 +20,53 @@ vim.api.nvim_create_user_command('InspectHl', function(opts)
 end, {
    nargs = 1,
    complete = 'highlight',
+})
+
+-- set the tab options for specific work repos
+vim.api.nvim_create_user_command('Work', function(opts)
+   local augroup_name = 'config.cmd.work'
+   ---@param val number
+   ---@param bufnr number
+   local set_opts = function(val, bufnr)
+      vim.bo[bufnr].shiftwidth = val
+      vim.bo[bufnr].softtabstop = val
+      vim.bo[bufnr].tabstop = val
+   end
+
+   if opts.args == 'on' then
+      set_opts(2, vim.api.nvim_get_current_buf())
+      vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufReadPost' }, {
+         group = vim.api.nvim_create_augroup(augroup_name, { clear = true }),
+         pattern = '*.{js,ts,md,css,json,mjs,cjs}',
+         desc = 'Set tabstop, shiftwidth to 2 for js,md,ts files',
+         callback = function(args)
+            set_opts(2, args.buf)
+         end,
+      })
+   elseif opts.args == 'off' then
+      set_opts(3, vim.api.nvim_get_current_buf())
+      vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+         group = vim.api.nvim_create_augroup(augroup_name, { clear = true }),
+         pattern = '*.{js,ts,md,css,json,mjs,cjs}',
+         desc = 'Revert tabstop, shiftwidth to 3 for js,md,ts files',
+         callback = function(args)
+            set_opts(3, args.buf)
+         end,
+      })
+   else
+      log.warn('config.cmds', 'Invalid Subcommand!')
+   end
+end, {
+   nargs = 1,
+   complete = function(_, line)
+      local subcommands = { 'on', 'off' }
+      local args = vim.split(line, '%s+')
+      if #args ~= 2 then
+         return {}
+      end
+
+      return vim.tbl_filter(function(subcommand)
+         return vim.startswith(subcommand, args[2])
+      end, subcommands)
+   end,
 })
