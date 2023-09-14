5abef7cfafe9a0588a8c4aad0526147f668eec31
Kevin Silvester <kevinsilvester05@gmail.com>
2023-09-14 21:18:13 +0100

    feat(scripts/git-hooks): create coloured output macro

--- scripts/git-hooks/src/lib.rs
+++ scripts/git-hooks/src/lib.rs
@@ -1,2 +1,17 @@
-pub mod paths;
+pub use ansi_term::Colour::RGB;
+
+#[macro_export]
+macro_rules! c_println {
+    (red, $($arg:tt)*) => {{
+        eprintln!("{}", $crate::RGB(235, 66, 66).paint($($arg)*))
+    }};
+    (blue, $($arg:tt)*) => {{
+        println!("{}", $crate::RGB(2, 149, 235).paint($($arg)*))
+    }};
+    (green, $($arg:tt)*) => {{
+        println!("{}", $crate::RGB(57, 219, 57).paint($($arg)*))
+    }};
+}
+
 pub mod hash;
+pub mod paths;
