265ec987dd3856d87d2018393f6ad9ceb9bd988f
Kevin Silvester <kevinsilvester05@gmail.com>
2023-09-16 17:28:00 +0100

    ci: update lint ci

--- scripts/git-hooks/src/paths.rs
+++ scripts/git-hooks/src/paths.rs
@@ -41,7 +41,7 @@ mod tests {
     fn test_paths() {
         let paths = Paths::new();
         assert!(paths.nvim_config.exists(), "nvim_config not found");
-        assert!(paths.nivm_data.exists(), "nvim_data not found" );
+        assert!(paths.nivm_data.exists(), "nvim_data not found");
         assert!(paths.git_hooks.exists(), "git_hooks not found");
         assert!(paths.ts_parsers.exists(), "ts_parsers not found");
     }
