7e16564967e981c464567dbd5feb323126c858e9
Kevin Silvester <kevinsilvester05@gmail.com>
2023-09-18 00:03:30 +0100

    feat(scripts): fix windows build script

--- scripts/nvim-utils/build.ps1
+++ scripts/nvim-utils/build.ps1
@@ -1,17 +1,18 @@
-$nvim-config = "$env:LOCALAPPDATA\nvim"
+$nvim_config = "$env:LOCALAPPDATA\nvim"
 
-Remove-Item -Path "$nvim-config\.git\hooks\*"
-Remove-Item -Path "$nvim-config\scripts\bin\*"
+Remove-Item -Path "$nvim_config\.git\hooks\*"
+Remove-Item -Path "$nvim_config\scripts\bin\*"
+New-Item -Path "$nvim_config\scripts\bin\.gitkeep" -ItemType File
 
-cd "$nvim-config\scripts\nvim-utils"
+cd "$nvim_config\scripts\nvim-utils"
 
 cargo build --release
 
 # copy git hooks
-Copy-Item -Path ".\target\release\post-merge.exe" -Destination "$nvim-config\.git\hooks\post-merge.exe"
+Copy-Item -Path ".\target\release\post-merge.exe" -Destination "$nvim_config\.git\hooks\post-merge.exe"
 
 # copy other binaries
-Copy-Item -Path ".\target\release\bootstrap.exe" -Destination "$nvim-config\scripts\bin\bootstrap.exe"
-# Copy-Item -Path ".\target\release\ts-parsers-download.exe" -Destination "$nvim-config\scripts\bin\ts-parsers-download.exe"
-# Copy-Item -Path ".\target\release\ts-parsers-compile.exe" -Destination "$nvim-config\scripts\bin\ts-parsers-compile.exe"
-# Copy-Item -Path ".\target\release\ts-parsers-compile-local.exe" -Destination "$nvim-config\scripts\bin\ts-parsers-compile-local.exe"
+Copy-Item -Path ".\target\release\bootstrap.exe" -Destination "$nvim_config\scripts\bin\bootstrap.exe"
+# Copy-Item -Path ".\target\release\ts-parsers-download.exe" -Destination "$nvim_config\scripts\bin\ts-parsers-download.exe"
+# Copy-Item -Path ".\target\release\ts-parsers-compile.exe" -Destination "$nvim_config\scripts\bin\ts-parsers-compile.exe"
+# Copy-Item -Path ".\target\release\ts-parsers-compile-local.exe" -Destination "$nvim_config\scripts\bin\ts-parsers-compile-local.exe"
