948fee6de38ca105633fb8adc557990617285ab2
Kevin Silvester <kevinsilvester05@gmail.com>
2023-09-20 14:13:39 +0100

    fix(scripts/nvim-utils): fix git hook not working on windows

--- scripts/nvim-utils/nu-git-hooks/src/bin/post-merge.rs
+++ scripts/nvim-utils/nu-git-hooks/src/bin/post-merge.rs
@@ -1,6 +1,12 @@
 use nu_git_hooks::hash::{Hash, NvimUtils};
 use nu_lib::{c_println, command::run_command, paths::Paths};
-use tokio::runtime;
+use tokio::{fs, runtime};
+
+#[cfg(windows)]
+const BIN_NAME: &str = "post-merge.exe";
+
+#[cfg(unix)]
+const BIN_NAME: &str = "post-merge";
 
 fn main() {
     let paths = Paths::new();
@@ -21,7 +27,13 @@ fn main() {
             c_println!(red, "       Run `bootstrap` first");
         }
 
-        c_println!(blue, "Checking ts-parsers hash...");
+        if paths.nvim_utils.join("target").is_dir() {
+            c_println!(blue, "\nDeleting target directory...");
+            fs::remove_dir_all(paths.nvim_utils.join("target")).await.unwrap();
+            c_println!(green, "Deleted target directory!");
+        }
+
+        c_println!(blue, "\nChecking nvim-utils hash...");
         let is_same = hash
             .compare_dir_hash(&NvimUtils, &paths.nvim_utils)
             .await
@@ -44,7 +56,6 @@ fn main() {
         {
             Ok(_) => (),
             Err(e) => {
-                dbg!(&e);
                 c_println!(red, "nvim-utils: Error rebuilding: {}", e);
                 return;
             }
@@ -62,7 +73,7 @@ fn main() {
             .nvim_utils
             .join("target")
             .join("release")
-            .join("post-merge");
+            .join(BIN_NAME);
         self_replace::self_replace(new_bin).unwrap();
         c_println!(green, "nvim-utils: Git hooks updated successfully!");
         c_println!(green, "nvim-utils: Done! (●'◡'●)");
