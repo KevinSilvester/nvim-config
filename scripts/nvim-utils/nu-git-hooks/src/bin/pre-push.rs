5f017d693c29fb950a5bb368e7627d7078d6edaa
Kevin Silvester <kevinsilvester05@gmail.com>
2024-01-01 19:41:19 +0000

    feat(nu/git-hooks): fix command status exit code not checked on pre-push

--- scripts/nvim-utils/nu-git-hooks/src/bin/pre-push.rs
+++ scripts/nvim-utils/nu-git-hooks/src/bin/pre-push.rs
@@ -11,26 +11,28 @@ async fn main() -> anyhow::Result<()> {
     check_command_exists("luacheck")?;
     check_command_exists("cargo")?;
 
-    if let Err(e) = run_command("stylua", &["--check", "./"], Some(&paths.nvim_config)).await {
+    pass_or_bail(run_command("stylua", &["--check", "./"], Some(&paths.nvim_config)).await)?;
+    pass_or_bail(run_command("luacheck", &["init.lua", "lua/*"], Some(&paths.nvim_config)).await)?;
+    pass_or_bail(
+        run_command(
+            "cargo",
+            &["fmt", "--all", "--", "--check"],
+            Some(&paths.nvim_utils),
+        )
+        .await,
+    )?;
+    pass_or_bail(run_command("cargo", &["test"], Some(&paths.nvim_utils)).await)?;
+
+    Ok(())
+}
+
+fn pass_or_bail(res: anyhow::Result<bool>) -> anyhow::Result<()> {
+    if let Err(e) = res {
         anyhow::bail!(e)
     }
 
-    if let Err(e) = run_command("luacheck", &["init.lua", "lua/"], Some(&paths.nvim_config)).await {
-        anyhow::bail!(e)
-    }
-
-    if let Err(e) = run_command(
-        "cargo",
-        &["fmt", "--all", "--", "--check"],
-        Some(&paths.nvim_utils),
-    )
-    .await
-    {
-        anyhow::bail!(e)
-    }
-
-    if let Err(e) = run_command("cargo", &["test"], Some(&paths.nvim_utils)).await {
-        anyhow::bail!(e)
+    if !res? {
+        anyhow::bail!("Command failed")
     }
 
     Ok(())
